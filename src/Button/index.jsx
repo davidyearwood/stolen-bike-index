e98913208cb3c4ff4a26dcf9035a122f80029d79
Davidood <dwhywood@gmail.com>
2019-04-06 17:13:49 -0500

    Create Button component

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./btn.css";
 
-function Button({ children, onClick }) {
-  return <button type="button" className={styles.btn} onClick={onClick}>{children}</button>;
+function Button({ children }) {
+  return <button type="button" className={styles.btn}>{children}</button>;
 }
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
