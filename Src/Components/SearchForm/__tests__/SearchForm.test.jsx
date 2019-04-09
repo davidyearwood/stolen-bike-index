a78168255df1f6d76d2935027d4bd26d8e082e53
Davidood <dwhywood@gmail.com>
2019-04-09 10:56:40 -0500

    Create search form component

--- Src/Components/SearchForm/__tests__/SearchForm.test.jsx
+++ Src/Components/SearchForm/__tests__/SearchForm.test.jsx
@@ -3,7 +3,25 @@ import { render } from "react-testing-library";
 import SearchForm from "../index";
 
 test("It should render a search description field", () => {
-  const { queryByPlaceholderText } = render(<SearchForm placeholder="Search case descriptions"/>);
+  const { queryByPlaceholderText } = render(<SearchForm />);
 
-  expect(queryByPlaceholderText("Search case for description")).toBeVisible();
+  expect(queryByPlaceholderText("Search case descriptions")).toBeVisible();
+});
+
+test("It should render a 'From' field", () => {
+  const { queryByPlaceholderText } = render(<SearchForm />);
+
+  expect(queryByPlaceholderText("From")).toBeVisible();
+});
+
+test("It should render a 'To' field", () => {
+  const { queryByPlaceholderText } = render(<SearchForm />);
+
+  expect(queryByPlaceholderText("To")).toBeVisible();
+});
+
+test("It should render a 'Find Cases' button", () => {
+  const { queryByText } = render(<SearchForm />);
+
+  expect(queryByText("Find Cases")).toBeVisible();
 });
---
+++ Src/Components/SearchForm/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "../Button";
+import Input from "../Input";
+import CalendarIcon from "../Svg/Calendar";
+import styles from "./searchForm.css";
+
+function SearchForm() {
+  return (
+    <form method="GET" action="/" className={styles.l_row}>
+      <div className={styles.l_col5}>
+        <Input label="Search case descriptions" placeholder="Search case descriptions" />
+      </div>
+      <div className={`${styles.l_col2} ${styles.inputWithIcon}`}>
+        <Input label="From" placeholder="From" />
+        <span className={styles.icon}><CalendarIcon height={30} width={30}/></span>
+      </div>
+      <div className={`${styles.l_col2} ${styles.inputWithIcon}`}>
+        <Input label="To" placeholder="To" />
+        <span className={styles.icon}><CalendarIcon height={30} width={30}/></span>
+      </div>
+      <div className={styles.l_col2}>
+        <Button>Find Cases</Button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
