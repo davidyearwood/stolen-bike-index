4b31cef2fc1d9354f2affbecd44aae068a7112f0
Davidood <dwhywood@gmail.com>
2019-04-11 18:14:21 -0500

    Generate # of buttons proportional to buttonsCount

--- src/Components/Pagination/__tests__/Pagination.test.jsx
+++ src/Components/Pagination/__tests__/Pagination.test.jsx
@@ -2,44 +2,32 @@ import React from "react";
 import { render } from "react-testing-library";
 import Pagination from "../index";
 
-test("It should render a 'First' button", () => {
-  const { queryByText } = render(<Pagination />);
-
-  expect(queryByText("First")).toBeVisible();
-});
-
 test("It should render a 'Prev' button", () => {
-  const { queryByText } = render(<Pagination />);
+  const { queryByText } = render(<Pagination buttonsCount={3} />);
 
-  expect(queryByText("Prev")).toBeVisible();
+  expect(queryByText("Previous")).toBeVisible();
 });
 
 test("It should render a 'Next' button", () => {
-  const { queryByText } = render(<Pagination />);
+  const { queryByText } = render(<Pagination buttonsCount={3} />);
 
   expect(queryByText("Next")).toBeVisible();
 });
 
-test("It should render a 'Last' button", () => {
-  const { queryByText } = render(<Pagination />);
-
-  expect(queryByText("Last")).toBeVisible();
-});
-
 test("It should render a '1' button", () => {
-  const { queryByText } = render(<Pagination />);
+  const { queryByText } = render(<Pagination buttonsCount={3} />);
 
   expect(queryByText("1")).toBeVisible();
 });
 
 test("It should render a '2' button", () => {
-  const { queryByText } = render(<Pagination />);
+  const { queryByText } = render(<Pagination buttonsCount={3} />);
 
   expect(queryByText("2")).toBeVisible();
 });
 
 test("It should render a '3' button", () => {
-  const { queryByText } = render(<Pagination />);
+  const { queryByText } = render(<Pagination buttonsCount={3} />);
 
   expect(queryByText("3")).toBeVisible();
 });
--- src/Components/Pagination/index.jsx
+++ src/Components/Pagination/index.jsx
@@ -1,46 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
 import gridStyles from "../../styles/grid.css";
 import Button from "../Button";
 import AngleLeftIcon from "../Svg/AngleLeft";
 import AngleRightIcon from "../Svg/AngleRight";
-import DoubleLeftIcon from "../Svg/DoubleLeft";
-import DoubleRightIcon from "../Svg/DoubleRight";
 import styles from "./pagination.css";
 
 const size = 20;
 
-function Pagination() {
+const generateButtons = (count) => {
+  const buttons = [];
+  for (let i = 1; i <= count; i += 1) {
+    buttons.push(
+      <li className={gridStyles.l_col1} key={`buttonNo_${i}`}>
+        <Button>{i}</Button>
+      </li>,
+    );
+  }
+
+  return buttons;
+};
+function Pagination({ buttonsCount }) {
   return (
     <ul className={`${gridStyles.l_row} ${styles.pagination}`}>
       <li className={gridStyles.l_col2}>
         <Button>
-          <DoubleLeftIcon height={size} width={size} fill="#ffffff" />
-          First
-        </Button>
-      </li>
-      <li className={gridStyles.l_col2}>
-        <Button>
           <AngleLeftIcon height={size} width={size} fill="#ffffff" />
-          Prev
+          Previous
         </Button>
       </li>
-      <li className={gridStyles.l_col1}><Button>1</Button></li>
-      <li className={gridStyles.l_col1}><Button>2</Button></li>
-      <li className={gridStyles.l_col1}><Button>3</Button></li>
+      {generateButtons(buttonsCount)}
       <li className={gridStyles.l_col2}>
         <Button>
           Next
           <AngleRightIcon height={size} width={size} fill="#ffffff" />
         </Button>
       </li>
-      <li className={gridStyles.l_col2}>
-        <Button>
-          Last
-          <DoubleRightIcon height={size} width={size} fill="#ffffff" />
-        </Button>
-      </li>
     </ul>
   );
 }
+
+Pagination.propTypes = {
+  buttonsCount: PropTypes.number.isRequired,
+};
 
 export default Pagination;
